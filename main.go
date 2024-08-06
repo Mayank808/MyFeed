79f3e4c180d5e5ef6ad1199fba6f115afb5301d6
Mayank808 <mayankmehra628@gmail.com>
2024-08-05 21:30:05 -0400

    feat: setup chi, added basic cors, setup hello world route

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment variables.")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins:   []string{"http://*", "https://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"*"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300,
+		},
+	))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    fmt.Sprintf(":%s", port),
+	}
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	log.Printf("Server started running on port %s...\n", port)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Error occured when on port %s: %v\n", port, err)
+	}
+}
